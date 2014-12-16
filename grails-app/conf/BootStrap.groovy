c00cd3966e710b6470203f1002a4fbc10629a7ce
ArmandoPrieto <prieto.arm@gmail.com>
2014-12-16 15:22:47 -0800

    Heroku Config

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -15,6 +15,17 @@ class BootStrap {
 				Locale.setDefault(Locale.ENGLISH);
 				println("Starting")
 			}
+				production{
+					def a = new Menu(title:"Home", url:"",  active:true, subMenu: null).save(flush: true)
+					def b = new Menu(title:"About", url:"",  active:false, subMenu: null).save(flush: true)
+					//def c = new Menu(title:"Home1", url:"",  active:false, subMenu: null).save(flush: true)
+					def d = new Menu(title:"Home2", url:"",  active:false, subMenu: [a,b]).save(flush: true)
+					
+					//Locale language
+					Locale.setDefault(Locale.ENGLISH);
+					println("Starting APP")
+					
+				}
 		}
 		
 		
